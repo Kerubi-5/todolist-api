462f395b3a8efd38c6e783a2762becc520194f54
John Kim A. Querobines <querobinesj@gmail.com>
2022-02-22 19:55:53 +0800

    adds a post method

--- controller/homeController.js
+++ controller/homeController.js
@@ -1,3 +1,8 @@
 exports.index = (req, res) => {
   res.json({ msg: "The start of the app" });
 };
+
+exports.store = (req, res) => {
+  const arr = { ...req.body };
+  res.json({ ...arr });
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require("express");
 const router = express.Router();
 const homeController = require("../controller/homeController");
 
-router.get("/", homeController.index);
+router.route("/").get(homeController.index).post(homeController.store);
 
 module.exports = router;
